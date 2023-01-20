77c72002d7a22f75c4e36a2117a565bd738dccda
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-01-20 10:38:56 +0100

    feat: withAuth function implementation.

--- lost-in-translation/src/components/Login/LoginForm.jsx
+++ lost-in-translation/src/components/Login/LoginForm.jsx
@@ -1,18 +1,26 @@
 import React, {useState} from "react";
+import { Navigate } from "react-router-dom";
 
 // Component for login
 function LoginForm() {
 
     let [username, setUsername] = useState("");
 
+    function login(){
+        localStorage.setItem("user", username);
+        localStorage.setItem("auth", true);
+
+        return <Navigate to="/translate"/>
+    }
+
     function onUsernameInput(event){
         setUsername(event.target.value);
-        console.log(username);
+        console.log(event.target.value);
     }
 
     return (<>
-        <h1>Login</h1>
         <input type="text" onChange={onUsernameInput} />
+        <button onClick={login} styke={{width:"200px", height:"140px"}}> Login </button>
     </>);
 }
 
