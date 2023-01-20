20548b6ee75df7c5e9a2b12675029108923fda27
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-01-20 11:04:19 +0100

    add: added assets and translate button

--- lost-in-translation/src/view-pages/Translate.jsx
+++ lost-in-translation/src/view-pages/Translate.jsx
@@ -1,12 +1,13 @@
 // Translator
 // import WithAuth from "";
-// import Translator from "../components/Translator/TranslatorForm"; # Translator component
+import TranslateToSign from "../components/Translator/TranslatorForm"; // Translator component
 
 function TranslatePage(){
-    //console.log("Entering translatorpage")
+    console.log("Entering translatorpage")
     return(
         <div>
             <h1> Translatorpage </h1>
+            <TranslateToSign/>
         </div>
     )
 };
