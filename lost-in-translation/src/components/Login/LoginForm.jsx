7eeec32817db574801fcc1d885cca4442179eadf
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-01-25 14:37:31 +0100

    refactor: Removed legacy code, and utilized the APIUtils file.

--- lost-in-translation/src/components/Login/LoginForm.jsx
+++ lost-in-translation/src/components/Login/LoginForm.jsx
@@ -1,126 +1,71 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { UserContext } from "../Contexts/UserContext";
+import { getUser, createNewUser, deleteUser } from "../../APIUtils";
 
 // Component for login
 function LoginForm() {    
 
     let [username, setUsername] = useState("");
+    const [user, setUser] = useContext(UserContext);
     const navigation = useNavigate();   
-    const apiURL = 'https://lostintranslation-lb-api.glitch.me';
-    const apiKey = 'noA+jgBPTEaCgn63IMJlGw==';
 
-    const createNewUser = async (name) =>
+    async function login(user)
     {
-        try{
-        const result = await fetch(`
-            ${apiURL}/translations`, 
-            {
-            method: 'POST',
-            headers: {
-            'X-API-Key': apiKey,
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 
-                username: name, 
-                translations: [] 
-            })
-            });
-
-            if(result.ok)
-            {
-                console.log("user " + username + " created successfully!");
-            }
-            else
-            {
-                console.log("failed to create user: " + username + "!");
-            }
-        }
-        catch(error)
-        {
-            console.log(error);
-        }
-    }
-
-    const userExists = async (name) =>{
-        const result = await fetch(`${apiURL}/translations?username=${name}`);
-        const jsonResult = await result.json();
-
-        if(jsonResult.length == 0)
-        {
-            return {"exists":false, "response": null};
-        }
-        else
-        {   
-            return {"exists":true, "response": jsonResult};         
-        }
-    }
-
-    async function login(){   
-                
-        const response = await userExists(username);        
-
-        if(response.exists)
-        {
-            //User found, proceed to login
-            //TODO: Save username somewhere?     
-            localStorage.SetItem("user")
+        const result = await getUser(user);      
+        
+        if(result.ok)
+        { 
+            setUser({username:user, id: result.response.id});  
+            localStorage.setItem("user", user);
+            navigation("/translate");
         }
         else
         {            
             //No matching user, create new user
-            createNewUser(username);
+            await createNewUser(user);
+            localStorage.setItem("user", user);
+            navigation("/translate");
         }
-
-        localStorage.setItem()
     }
 
-    const Delete = () =>{
-        const apiURL = 'https://lostintranslation-lb-api.glitch.me';
-        const apiKey = 'noA+jgBPTEaCgn63IMJlGw==';
-        const userId = parseInt(username);
+    async function btnLogin(){   
+        await login(username);
+    }       
 
-        fetch(`${apiURL}/translations/${userId}`, {
-            method: 'DELETE', // NB: Set method to PATCH
-            headers: {
-                'X-API-Key': apiKey,
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({})
-        })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Could not update translations history')
-          }
-          return response.json()
-        })
-        .then(updatedUser => {
-          // updatedUser is the user with the Patched data
-          console.log("deleted user " + username);
-        })
-        .catch(error => {
-        })
+    async function btnDelete(){
+        await deleteUser(username);
     }
-
-    
 
     function onUsernameInput(event){
         setUsername(event.target.value);
-        console.log(event.target.value);
     }    
 
-    useEffect(()=>{
-        if(userExists())
+    async function autoLogin(user){   
+        if(localStorage.getItem("user") != null)
         {
-            navigation("/translate");
+            let result = await getUser(user);
+
+            if(result.ok)
+            {
+                setUser({username:user, id:result.response.id});
+                localStorage.setItem("user", user);
+                navigation("/translate");
+            }
         }
-    }, []);
+    }
+
+    useEffect(()=>{
+        console.log("Local storage user : " + localStorage.getItem("user"));
+        autoLogin(localStorage.getItem("user"));
+    }, [user]);
     
 
     return (<>        
         <input type="text" onChange={onUsernameInput} />
-        <button onClick={login} style={{width:"80px", height:"20px"}}> Login </button>
-        <button onClick={Delete}  style={{width:"80px", height:"20px"}}> Delete </button>
+        <button onClick={btnLogin} style={{width:"80px", height:"20px"}}> Login </button>
+        <button onClick={btnDelete}  style={{width:"80px", height:"20px"}}> Delete </button>
     </>);
 
     //
