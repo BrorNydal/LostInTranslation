48ba6d7bce247f17a8461de1cb95afcdb1b2e547
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-01-25 15:53:46 +0100

    chore: Profile now uses authorization.

--- lost-in-translation/src/view-pages/Profile.jsx
+++ lost-in-translation/src/view-pages/Profile.jsx
@@ -1,16 +1,17 @@
 // Profile with history
-import TranslateReport from "../components/Profile/ProfileHistory";
-import withAuth from "../components/Login/WithAuth";
+import TranslateReport from "../Components/Profile/ProfileHistory";
+import Authorizer from "../Components/Login/Authorizer";
 
 function ProfilePage(){
     return(
         <div>
             <h1> ProfilePage </h1>
+            <Authorizer/>
             <TranslateReport/>
         </div>
     )
 };
 
-export default withAuth(ProfilePage);
+export default ProfilePage;
 
 // <!--  -->
