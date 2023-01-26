957fc2dd658fe6167d40df2f500b4d0e4739e6e8
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-01-26 16:44:25 +0100

    feat: Logout user.

---
+++ lost-in-translation/src/Components/Logout/Logout.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../Contexts/UserContext";
+
+
+function Logout(){
+    const [user, setUser] = useContext(UserContext);
+    const navigator = useNavigate();
+
+    function logout(){
+        localStorage.setItem("user", null);
+        setUser({username:"", id:0});
+        navigator("/");
+    }
+
+    return (<>
+        <button onClick={logout}> Logout </button>
+    </>);
+}
+
+export default Logout;
--- lost-in-translation/src/view-pages/Profile.jsx
+++ lost-in-translation/src/view-pages/Profile.jsx
@@ -1,13 +1,15 @@
 // Profile with history
 import HistoryLog from "../Components/Profile/ProfileHistory";
 import Authorizer from "../Components/Login/Authorizer";
+import Logout from "../Components/Logout/Logout";
 
 function ProfilePage(){
     return(
         <div>
             <Authorizer/>
             <h1> ProfilePage </h1>            
-            <HistoryLog/>
+            <HistoryLog/>  
+            <Logout/>          
         </div>
     )
 };
