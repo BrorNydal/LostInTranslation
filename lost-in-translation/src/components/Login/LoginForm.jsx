4b6bff4474b49fcdbad070da0f6ff2e4571d9b71
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-01-27 09:36:29 +0100

    fix: Valid username check.

--- lost-in-translation/src/components/Login/LoginForm.jsx
+++ lost-in-translation/src/components/Login/LoginForm.jsx
@@ -9,10 +9,35 @@ function LoginForm() {
 
     let [username, setUsername] = useState("");
     const [user, setUser] = useContext(UserContext);
-    const navigation = useNavigate();   
+    const navigation = useNavigate(); 
+    
+    const maxUserNameLength = 33;
+    let errorMessage = "";
+
+    function ErrorMessage(){
+        if(errorMessage.length <= 0)
+        {
+            return <></>;
+        }
+        else
+        {
+            return <><p style={{color: "red"}}>{errorMessage}</p></>
+        }
+    }
 
     async function login(user)
     {
+        if(user.length <= 0)
+        {
+            errorMessage = "Username must consist of at least one character!";
+            return;
+        }
+        else if(user.length >= maxUserNameLength)
+        {
+            errorMessage = `Username must be shorter than ${maxUserNameLength} characters!`;
+            return;
+        }
+
         const result = await getUser(user);      
         
         if(result.ok)
@@ -34,6 +59,10 @@ function LoginForm() {
     async function btnLogin(){   
         await login(username);
     }     
+
+    async function btnDelete(){
+        await deleteUser(username);
+    }
 
     function onUsernameInput(event){
         setUsername(event.target.value);
@@ -61,9 +90,11 @@ function LoginForm() {
 
     return (<>        
         <input type="text" onChange={onUsernameInput} />
-        <button onClick={btnLogin} style={{width:"80px", height:"20px"}}> Login </button>
+        <ErrorMessage/>
+        <button onClick={btnLogin} style={{width:"80px", height:"20px"}}> Login </button>     
+        
     </>);
-
+    //<button onClick={btnDelete} style={{width:"80px", height:"20px"}}> Delete </button>      
     //
 }
 
